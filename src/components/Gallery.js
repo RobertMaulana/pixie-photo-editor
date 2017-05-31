b1d64387f1ff3719ffce7e95d9661486836ae254
maulana <maulana.robert.mr@gmail.com>
2017-05-31 20:38:21 +0700

    add modal ready to edit photo

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import {
-  View,
-  Text,
   CameraRoll,
   ScrollView,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  Modal,
+  View,
+  Text,
+  Button,
+  Dimensions
 } from "react-native";
-
+let {height, width} = Dimensions.get('window');
 class Gallery extends Component {
   constructor (props){
     super(props);
     this.state = {
-      photos: []
+      photos: [],
+      editor: false,
+      photo: ""
     }
   }
 
@@ -23,16 +28,21 @@ class Gallery extends Component {
     }).then(r => this.setState({photos: r.edges}))
 
   }
+  editor(uriPicture){
+    this.setState({editor: !this.state.editor})
+    this.setState({photo: uriPicture})
+  }
 
   render(){
-    const { photos } = this.state
-    const { imageContainerStyle, imageStyle } = styles
+    const { photos } = this.state;
+    const { imageContainerStyle, imageStyle, imageEditorStyle, imageEditorContainerStyle, modalButtonStyle } = styles;
+
     return (
       <ScrollView>
       {(photos !== null) && (
         photos.map((dataPhoto) => {
           return (
-            <TouchableOpacity key={dataPhoto.node.image.uri}>
+            <TouchableOpacity key={dataPhoto.node.image.uri} onPress={() => this.editor(dataPhoto.node.image.uri)}>
               <Image
                 resizeMode={Image.resizeMode.cover}
                 style={imageStyle}
@@ -42,6 +52,26 @@ class Gallery extends Component {
           )
         })
       )}
+      <Modal
+        animationType={"slide"}
+        transparent={false}
+        visible={this.state.editor}
+        onRequestClose={() => {alert("Modal has been closed!")}}
+      >
+        <View>
+          <Image
+            resizeMode={Image.resizeMode.cover}
+            style={imageEditorStyle}
+            source={{uri: this.state.photo}}
+          />
+          <Button
+            onPress={() => this.setState({editor: false})}
+            title="Cancel"
+            color="#841584"
+            style={modalButtonStyle}
+          />
+        </View>
+      </Modal>
       </ScrollView>
     )
   }
@@ -52,6 +82,18 @@ const styles = {
     flex: 1,
     width: 150,
     height: 150
+  },
+  imageEditorContainerStyle: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  imageEditorStyle: {
+    height: height - 100,
+    width: 600
+  },
+  modalButtonStyle: {
+    height: 100,
+    fontSize: 40
   }
 }
 
